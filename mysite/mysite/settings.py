385cfb2354d987a1e7ed4c5ea9eb8bfd24838e91
popov <piligrimqwert@gmail.com>
2024-06-20 14:53:21 +0300

    Updated gitignore file and Language Code

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -116,11 +116,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uk'
 
 TIME_ZONE = 'Europe/Kiev'
 
-USE_I18N = False
+USE_I18N = True
 
 USE_TZ = True
 
